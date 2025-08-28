7e0efa737b4a16c85b6b4ad67c0dcaa8e373b6f3
vasilii <v.verkin@gmail.com>
2025-08-28 17:07:53 +0300

    Добавляет несколько компанентов

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { NewPointButtonView } from './view/add-new-point-view.js';
+import { FilterView } from './view/filter-view.js';
+import { InfoTripView } from './view/info-trip-view.js';
 import { render } from './render.js';
 // Найдем main разметки
 // const siteMainElement = document.querySelector('.main');
 
 const siteHeader = document.querySelector('.page-header');
+const siteHeaderMainElement = document.querySelector('.trip-main');
 const siteHeaderElement = siteHeader.querySelector('.trip-filters');
+const siteHeaderFilter = siteHeaderMainElement .querySelector('.trip-controls__filters');
 
-render (new NewPointButtonView(), siteHeaderElement);
+render (new InfoTripView(), siteHeaderMainElement);
+// render (new NewPointButtonView(), siteHeaderElement);
+render (new FilterView(), siteHeaderFilter);
