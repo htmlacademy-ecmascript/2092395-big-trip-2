402fd9be079ae87ecd58cecfc5b0f0553ef5f167
vasilii <v.verkin@gmail.com>
2025-09-03 17:18:32 +0300

    добавляет моки

---
+++ src/mock/destinations.js
@@ -0,0 +1,35 @@
+export const mockDestinations = [
+  {
+    id: 'cfe416cq-10xa-ye10-8077-2fs9a01edcab',
+    description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Chamonix',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building',
+      }
+    ]
+  },
+  {
+    id: 'cfe416cq-10xa-ye10-8077-2fs9a01edcac',
+    description: 'Amsterdam, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Amsterdam',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Amsterdam parliament building',
+      }
+    ]
+  },
+  {
+    id: 'cfe416cq-10xa-ye10-8077-2fs9a01edcad',
+    description: 'Geneva, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Geneva',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Geneva parliament building',
+      }
+    ]
+  },
+];
--- src/mock/points.js
+++ src/mock/points.js
@@ -3,13 +3,40 @@ import { TYPE_OF_EVENTS } from '../const.js';
 
 const mockPoints = [
   {
-    tripDate: new Date(),
-    event: getRandomArrayElement(TYPE_OF_EVENTS),
-    startTime: null,
-    endTime: null,
-    price: 40,
-    offers: [],
-    isFavourite: false,
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808c',
+    basePrice: 1100,
+    dateFrom: '2019-07-10T20:55:56.845Z',
+    dateTo: '2019-07-10T21:22:13.375Z',
+    destination: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+    isFavorite: false,
+    offers: [
+      'b4c3e4e6-9053-42ce-b747-e281314baa31',
+    ],
+    type: 'taxi',
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808d',
+    basePrice: 1200,
+    dateFrom: '2019-07-11T22:55:56.845Z',
+    dateTo: '2019-07-11T11:22:13.375Z',
+    destination: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e05',
+    isFavorite: false,
+    offers: [
+      'b4c3e4e6-9053-42ce-b747-e281314baa31',
+    ],
+    type: 'taxi',
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808e',
+    basePrice: 1300,
+    dateFrom: '2019-07-12T22:55:56.845Z',
+    dateTo: '2019-07-13T11:22:13.375Z',
+    destination: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e06',
+    isFavorite: false,
+    offers: [
+      'b4c3e4e6-9053-42ce-b747-e281314baa31',
+    ],
+    type: 'taxi',
   },
 ];
 
